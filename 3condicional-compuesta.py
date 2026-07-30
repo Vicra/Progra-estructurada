6901b8b4fcfac522e53e09864e2bd0fcaf1c3325
Victor Ramirez <Vicramirez_10@hotmail.com>
2026-07-30 13:46:10 -0600

    Ejercicios clase 30 jul

---
+++ 3condicional-compuesta.py
@@ -0,0 +1,9 @@
+# declaracion de variables
+x = 15
+y = 7
+
+# condicional compuesta
+if x > 13 and y == 4:
+  print("La suma es ", x + y)
+else:
+  print("No se realizo la suma")
---
+++ 4parOimpar.py
@@ -0,0 +1,5 @@
+n = 10
+if n % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
