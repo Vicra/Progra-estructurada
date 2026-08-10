cde21ce3587a7603ad8982459f6eb879143dba65
Victor Ramirez <Vicramirez_10@hotmail.com>
2026-08-10 13:31:48 -0600

    For statement

---
+++ 15forSample.py
@@ -0,0 +1,21 @@
+listaDeVerduras = [
+  "brocoli"
+  ,"chile"
+  , "cebolla"
+  , "tomate"
+  , "zanahoria"
+  , "col"
+  , "ajo"
+]
+
+# listaDeVerduras.append("berenjena")
+
+listaDeVerduras.pop()
+
+for i in range(len(listaDeVerduras)):
+  print(listaDeVerduras[i])
+
+
+# len -> retorna el tamano de la estructura
+# length (param1)
+# length (lista)
