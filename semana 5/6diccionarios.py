dc0eead09e35ba8b7caed03b9d76c99cdb66600b
Victor Ramirez <Vicramirez_10@hotmail.com>
2026-08-25 13:06:49 -0600

    Create 6diccionarios.py

---
+++ semana 5/6diccionarios.py
@@ -0,0 +1,23 @@
+# DICCIONARIOS en py
+# Diccionarios son estructuras de datos que almacenan datos a traves de una llave/clave y un valor
+
+# 1. Crear diccionario
+estudiante = {
+  "name": "Victor",
+  "account": "21311096",
+  "average": 70,
+  "email": "victor"
+}
+
+# 2. Mostrar el valor (valores) de una llave del diccionario
+
+# 3. Modificar un valor de una llave del diccionario
+
+# 4. Agregar una nueva clave
+
+# 5. Mostrar todas las claves/llaves -> .keys()
+
+# 6. Mostrar todas los valores -> .values()
+
+# 7. Mostrar todos las llaves y valores -> a,b in items
+
